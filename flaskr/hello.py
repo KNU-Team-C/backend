d64c7bdf0b3958a2b4d68997ca90d30b805683fa
Ann Alieksieienko <ann.alieksieienko@gmail.com>
2023-02-14 12:29:32 +0200

    Update flask file

--- flaskr/hello.py
+++ flaskr/hello.py
@@ -5,4 +5,4 @@ bp = Blueprint("hello", __name__, url_prefix="/hello")
 
 @bp.route("/", methods=["GET"])
 def hello():
-    return "Hello! This is a mock endpoint! TEEEEST"
+    return "Hello! This is a mock endpoint! TEEEEST PIPELINE"
