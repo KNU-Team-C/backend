b356cbff499633a6b4399c9f713a256a3f95ba14
Anna Alieksieienko <55431139+annalieks@users.noreply.github.com>
2023-02-14 10:58:28 +0000

    Add endpoint to check automatic update via pipeline (#2)

--- flaskr/hello.py
+++ flaskr/hello.py
@@ -6,3 +6,8 @@ bp = Blueprint("hello", __name__, url_prefix="/hello")
 @bp.route("/", methods=["GET"])
 def hello():
     return "Hello! This is a mock endpoint! :)"
+
+
+@bp.route("/greeting", methods=["GET"])
+def hello():
+    return "Hello! This is yet another one test endpoint"
