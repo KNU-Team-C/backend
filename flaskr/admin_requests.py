3b44afeb1affc23e51e8e8e52c3060e278303983
DeLitX <dimkanan@gmail.com>
2023-03-20 20:28:36 +0200

    Split common function to two separate functions

--- flaskr/admin_requests.py
+++ flaskr/admin_requests.py
@@ -2,14 +2,13 @@ from flask import request, jsonify, make_response, Blueprint
 
 from flaskr.models import User, UserReport, Company, CompanyReport
 from flaskr.database import db
-from sqlalchemy import text
+from sqlalchemy import text, or_
 
 bp = Blueprint("admin_requests", __name__, url_prefix="/admin")
 
 
-# THIS REQUEST WORKS INCORRECTLY
-@bp.route('/requests', methods=['GET'])
-def get_requests():
+@bp.route('/requests_companies', methods=['GET'])
+def get_requests_companies():
     """
     Accepting following query parameters:
     types - types of items to get. Possible values: 'verification', 'reports', none.
@@ -17,89 +16,100 @@ def get_requests():
     page - number of the page
     page_size - size of each page
     search_query - search query for users
-    is_users_enabled - should include users into answer
-    is_companies_enabled - should include companies into answer
+
+    Returning companies that had been reported and/or are not verified (depending on types passed)
     """
     page = request.args.get('page', 1, type=int)
     page_size = request.args.get('page_size', 10, type=int)
     types = request.args.get('types', '', type=str)
     search_query = request.args.get('search_query', '', type=str)
-    is_users_enabled = request.args.get('is_users_enabled', "", type=str).lower() == "true"
-    is_companies_enabled = request.args.get('is_companies_enabled', "", type=str).lower() == "true"
 
-    companies_query = db.session.query(Company)
-    users_query = db.session.query(User)
+    query = db.session.query(Company)
+    filters = []
     if 'verification' in types or not types:
-        companies_query = companies_query.filter(not Company.is_verified)
+        filters.append(Company.is_verified == False)
     if 'reports' in types or not types:
-        companies_subquery = db.session.query(CompanyReport.company_id).filter(
+        subquery = db.session.query(CompanyReport.company_id).filter(
             CompanyReport.company_id == CompanyReport.id).exists()
-        companies_query = companies_query.filter(companies_subquery)
-        users_subquery = db.session.query(UserReport.reported_user_id).filter(
-            UserReport.reported_user_id == User.id).exists()
-        users_query = users_query.filter(users_subquery)
+        filters.append(subquery)
+
+    if len(filters) > 0:
+        query = query.filter(or_(*filters))
 
     if search_query:
-        users_query = users_query.filter(
-            text("first_name || ' ' || last_name LIKE :query").params(query='%' + search_query + '%'))
-        companies_query = companies_query.filter(text("name LIKE :query").params(query='%' + search_query + '%'))
+        query = query.filter(text("name LIKE :query").params(query='%' + search_query + '%'))
 
-    companies_query = companies_query.order_by(Company.date_created)
-    users_query = users_query.order_by(User.date_joined)
-    if is_users_enabled == is_companies_enabled:
-        query = users_query.join(companies_query, User.id != Company.id, isouter=True,
-                                 full=True)
-    elif is_users_enabled:
-        query = users_query
-    elif is_companies_enabled:
-        query = companies_query
-    else:
-        return jsonify({}), 200
+    query = query.order_by(Company.date_created)
 
-    items = query.paginate(page=page, per_page=page_size)
+    companies = query.paginate(page=page, per_page=page_size)
 
     result = {
-        'total_pages': items.pages,
-        'total_items': items.total,
-        'items_per_page': items.per_page,
-        'current_page': items.page,
-        'items': [map_item_to_response(item) for item in items.items]
-    }
-
-    return jsonify(result), 200
-
-
-def map_item_to_response(item):
-    print(item)
-    if type(item) is Company:
-        return {
-            'id': item.id,
+        'total_pages': companies.pages,
+        'total_items': companies.total,
+        'items_per_page': companies.per_page,
+        'current_page': companies.page,
+        'items': [{
+            'id': company.id,
             'type': 'Company',
-            'ava_url': item.logo_url,
-            'name': item.name,
-            'description': item.description,
+            'ava_url': company.logo_url,
+            'is_verified': company.is_verified,
+            'name': company.name,
+            'description': company.description,
             'industries': [{
                 'id': industry.id,
                 'name': industry.name
-            } for industry in flat_map(lambda project: project.industries, item.projects)],
+            } for industry in flat_map(lambda project: project.industries, company.projects)],
             'technologies': [{
                 'id': technology.id,
                 'name': technology.name
-            } for technology in flat_map(lambda project: project.technologies, item.projects)],
-        }
-    elif type(item) is User:
-        return {
-            'id': item.id,
+            } for technology in flat_map(lambda project: project.technologies, company.projects)],
+        } for company in companies.items]
+    }
+    return jsonify(result), 200
+
+
+@bp.route('/requests_users', methods=['GET'])
+def get_requests_users():
+    """
+    Accepting following query parameters:
+    page - number of the page
+    page_size - size of each page
+    search_query - search query for users
+
+    Returning users that had been reported
+    """
+    page = request.args.get('page', 1, type=int)
+    page_size = request.args.get('page_size', 10, type=int)
+    search_query = request.args.get('search_query', '', type=str)
+
+    subquery = db.session.query(UserReport.id).filter(UserReport.reported_user_id == UserReport.id).exists()
+    query = db.session.query(User).filter(subquery)
+
+    if search_query:
+        query = query.filter(text("name LIKE :query").params(query='%' + search_query + '%'))
+
+    query = query.order_by(User.date_joined)
+
+    users = query.paginate(page=page, per_page=page_size)
+
+    result = {
+        'total_pages': users.pages,
+        'total_items': users.total,
+        'items_per_page': users.per_page,
+        'current_page': users.page,
+        'items': [{
+            'id': user.id,
             'type': 'User',
-            'ava_url': item.ava_url,
-            'first_name': item.first_name,
-            'last_name': item.last_name,
+            'ava_url': user.ava_url,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
             'companies': [{
                 'id': company.id,
                 'name': company.name
-            } for company in item.companies]
-        }
-    print(str(item) + str(type(item) is User) + ' ' + str(type(item)) + 'not returned')
+            } for company in user.companies]
+        } for user in users.items]
+    }
+    return jsonify(result), 200
 
 
 flat_map = lambda f, xs: [y for ys in xs for y in f(ys)]
@@ -109,19 +119,18 @@ flat_map = lambda f, xs: [y for ys in xs for y in f(ys)]
 def get_filtered_requests():
     """
     Accepting following query parameters:
-    type - type of requests to get amount. Possible values: 'verification', 'reports', none
+    type - type of requests to get amount. Possible values: 'verification', 'reports'
     """
     type_arg = request.args.get("type")
-    if type_arg == "banned":
-        amount = db.session.query(User.id).filter_by(is_blocked=True).count()
-    elif type_arg == "reported":
+    if type_arg == "verification":
+        amount = db.session.query(Company.id).filter_by(is_verified=False).count()
+    elif type_arg == "reports":
         companies_subquery = db.session.query(CompanyReport.company_id).filter(
             CompanyReport.company_id == CompanyReport.id).exists()
         users_subquery = db.session.query(UserReport.reported_user_id).filter(
             UserReport.reported_user_id == User.id).exists()
         users_amount = db.session.query(User.id).filter(users_subquery).count()
         companies_amount = db.session.query(Company.id).filter(companies_subquery).count()
-        print(str(users_amount) + " " + str(companies_amount))
         amount = users_amount + companies_amount
     else:
         return make_response('Such type parameter is not allowed', 400)
