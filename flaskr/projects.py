920aa6616fa9f8b769b6a030bde5cfecbce27c5e
maxyudkin <maxyudkin@planeks.net>
2023-04-06 16:58:09 +0300

    put and delete company

--- flaskr/projects.py
+++ flaskr/projects.py
@@ -42,3 +42,61 @@ def create_project(current_user):
 
     response = project.get_info()
     return jsonify(response)
+
+
+@bp.route('/<project_id>', methods=['PUT'])
+def project_edit(project_id):
+    data = request.get_json()
+    query = db.session.query(Project)
+
+    project = query.filter(Project.id == project_id).first()
+    project.title = data['title']
+    project.url = data['url']
+    project.description = data['description']
+    project.logo_url = data['logo_url']
+    project.is_public = data['is_public']
+    project.company_id = data['company_id']
+    technologies_id = data['technologies']
+    industries_id = data['industries']
+    attachments_id = data['attachments']
+
+    new_techlogies = []
+    for technology_id in technologies_id:
+        technology = Technology.query.filter(Technology.id == technology_id).first()
+        if technology is not None:
+            new_techlogies.append(technology)
+    project.technologies = new_techlogies
+
+    new_industries = []
+    for industrie_id in industries_id:
+        industrie = Industry.query.filter(Industry.id == industrie_id).first()
+        if industrie is not None:
+            new_industries.append(industrie)
+    project.industries = new_industries
+
+    new_attachments = []
+    for attachment_id in attachments_id:
+        attachment = Attachment.query.filter(Attachment.id == attachment_id).first()
+        if attachment is not None:
+            new_attachments.append(attachment)
+    project.attachments = new_attachments
+
+    db.session.commit()
+
+    response = project.get_info()
+    return jsonify(response)
+
+
+@bp.route('/<project_id>', methods=['DELETE'])
+def project_delete(project_id):
+
+    project = Project.query.get(project_id)
+    db.session.delete(project)
+    db.session.commit()
+
+    response = {
+        "message": "delete completed!"
+    }
+    return jsonify(response)
+
+
